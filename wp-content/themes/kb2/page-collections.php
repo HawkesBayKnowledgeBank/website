69b6f261a174f9a3cad91d50ae8d49fb134e46be
ChrisWebbNZ <espiex@gmail.com>
2016-06-21 20:31:08 +1200

    Collections page

--- wp-content/themes/kb2/page-collections.php
+++ wp-content/themes/kb2/page-collections.php
@@ -3,33 +3,104 @@
 <div class="grid-container collections-list">
 
 
-
 	<?php
 
-		// List terms in a given taxonomy using wp_list_categories (also useful as a widget if using a PHP Code plugin)
-		 
-		$taxonomy     = 'collections';
-		$orderby      = 'name'; 
-		$show_count   = true;
-		$pad_counts   = false;
-		$hierarchical = true;
-		$title        = '';
-		 
-		$args = array(
-		  'taxonomy'     => $taxonomy,
-		  'orderby'      => $orderby,
-		  'show_count'   => $show_count,
-		  'pad_counts'   => $pad_counts,
-		  'hierarchical' => $hierarchical,
-		  'title_li'     => $title
-		);
+		//top level collections
+		$collections = get_terms( array(
+		    'taxonomy' => 'collections',
+		    'hide_empty' => false,
+		    'parent' => 0,
+		));		
+
 	?>
- 
-	<ul class="collections">
-	    <?php wp_list_categories( $args ); ?>
-	</ul>
+
+	<?php if(!empty($collections)): ?>
+
+		<?php foreach($collections as $collection): ?>
+
+			<div class="collection">
+
+				<?php if(get_field('image',$collection)): ?>
+
+					<?php $image = get_field('image', $collection); ?>
+
+					<?php print_r($image); ?>
+
+				<?php endif; ?>
+
+				<h3><a href="<?php echo get_term_link($collection->term_id); ?>"><?php echo $collection->name; ?></a></h3>
+				
+				<?php if(!empty($collection->description)): ?>
+
+					<div class="description">
+
+						<?php echo $collection->description; ?>
+
+					</div><!-- . description -->
+
+					<?php 
+						//children
+						$children = get_terms( array(
+						    'taxonomy' => 'collections',
+						    'hide_empty' => false,
+						    'parent' => $collection->term_id,
+						));	
+					?>
+
+					<?php if(!empty($children)): ?>
+
+						<div class="subcollections tiles">
+
+							<?php foreach($children as $child): ?>
+
+								<div class="subcollection tile">
+
+									<h4><a href="<?php echo get_term_link($child->term_id); ?>"><?php echo $child->name; ?></a></h4>
+
+								</div><!-- .subcollection -->
+
+							<?php endforeach; ?>
+
+						</div><!-- .subcollections -->
+
+					<?php endif; ?>
+
+				<?php endif; ?>
+
+			</div><!-- .collection -->
+
+		<?php endforeach; ?>
+
+	<?php endif; ?>
 
 
+<?php
+	/*
+			// List terms in a given taxonomy using wp_list_categories (also useful as a widget if using a PHP Code plugin)
+			 
+			$taxonomy     = 'collections';
+			$orderby      = 'name'; 
+			$show_count   = true;
+			$pad_counts   = false;
+			$hierarchical = true;
+			$title        = '';
+			 
+			$args = array(
+			  'taxonomy'     => $taxonomy,
+			  'orderby'      => $orderby,
+			  'show_count'   => $show_count,
+			  'pad_counts'   => $pad_counts,
+			  'hierarchical' => $hierarchical,
+			  'title_li'     => $title
+			);
+		?>
+	 
+		<ul class="collections">
+		    <?php wp_list_categories( $args ); ?>
+		</ul>
+
+	*/
+?>
 
 </div>
 
