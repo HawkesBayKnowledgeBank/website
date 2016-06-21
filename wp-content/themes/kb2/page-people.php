36a5f76fcb73c82ad41ed38300f3baa4199a6513
Gabriel Ward <gabrielward@gmail.com>
2016-06-21 15:49:30 +1200

    added function for calling an extra menu, use in header.php

--- wp-content/themes/kb2/page-people.php
+++ wp-content/themes/kb2/page-people.php
@@ -159,7 +159,9 @@
 						
 
 					<?php if ( $counter % 3 == 0 && $counter != $number_of_rows) : ?>
+        				
         				</div><div class="grid-container peopleNames">
+        			
         			<?php endif; ?>
 
 				<?php
