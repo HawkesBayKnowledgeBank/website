e72404e3dff645ae9539ae3a5333a7d5a3b821c4
Gabriel Ward <gabrielward@gmail.com>
2016-06-21 16:39:56 +1200

    added page-about-us.php

---
+++ wp-content/themes/kb2/page-about-us.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="pageTitles">
+	
+	<h1><?php the_title(); ?></h1>
+
+</div>
+
+<div class='aboutPageContent'>
+
+<?php the_content(); ?>
+
+</div>
+<?php get_footer(); ?>
