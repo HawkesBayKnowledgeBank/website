fc9fbda75f0343fa04f359ab324a0f3412db0f41
ChrisWebbNZ <espiex@gmail.com>
2016-06-27 20:44:59 +1200

    Image quickview links, tweaks to collections page

--- wp-content/themes/kb2/tile-video.php
+++ wp-content/themes/kb2/tile-video.php
@@ -15,7 +15,7 @@
 		<?php elseif(get_field('youtube_id',$post->ID)): ?>
 
 			<?php $ytid = get_field( 'youtube_id' ); ?>
-			<a href="<?php the_permalink(); ?>?quickview=true"><img src="http://img.youtube.com/vi/<?php echo $ytid; ?>/0.jpg" /></a>
+			<a href="<?php the_permalink(); ?>?quickview=true" class="quick_view"><img src="http://img.youtube.com/vi/<?php echo $ytid; ?>/0.jpg" /></a>
 
 			<span class="quick_view"><a class="lightbox_icon quick_view" href="<?php the_permalink(); ?>?quickview=true"><img src="/wp-content/themes/kb2/img/search-white.png" alt="View" /></a></span>		
 
