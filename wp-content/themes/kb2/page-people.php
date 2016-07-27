80b91cddaf1a918aac2d5bac48b6bc19633a26b9
Gabriel Ward <gabrielward@gmail.com>
2016-07-27 18:09:50 +1200

    changed images and films pages to automatic latest posts

--- wp-content/themes/kb2/page-people.php
+++ wp-content/themes/kb2/page-people.php
@@ -3,165 +3,138 @@
 <div class="grid-container">
 <div class="pageTitles">
 <h1><?php the_title(); ?></h1>
+<h3>Latest Posts</h3>
 </div>
-	<?php $feature_posts = get_field( 'content_feature_posts' );
-
-	if(isset( $feature_posts ) ) : ?>
-
-		<?php foreach( $feature_posts as $feature_post ) : ?>
-
-			<div class="grid-4">
-
-				<a href="<?php echo get_permalink( $feature_post['post_link']->ID ); ?>">
-
-					<img src="<?php echo $feature_post['image']['sizes']['300w']; ?>" alt="<?php echo $feature_page['image']['alt']; ?>">
-
-				</a>
-
-				<a href="<?php echo get_permalink( $feature_post['post_link']->ID ); ?>">
-
-					<h3><?php echo $feature_post['post_title']; ?></h3>
-
-				</a>
-
-			</div>
-
-<?php endforeach; ?>
-
-	<?php endif; ?>
 
 </div>
 
-	<div class="container">
+<?php
 
+	$args = array(
 
+		'post_type' => 'person',
 
+		'posts_per_page' => 5
 
-		<?php
-		//identify rows that have the matching first letter of family name with
-		//the letter that is clicked on
+	);
 
-		//set $letter to our GET variable if that variable is set, otherwise false
-		$letter = ( isset($_GET['letter']) ? $_GET['letter'] : false );
+	$latest_posts = get_posts($args);
 
+?>
 
-		if($letter) :
-
-			//match family_name on our letter, with both coverted to lowercase in case either is uppercase
-
-			$rows = $wpdb->get_results($wpdb->prepare(
-	            "
-	            SELECT post_id
-	            FROM {$wpdb->prefix}postmeta
-	            WHERE meta_key LIKE %s
-	                AND LOWER(meta_value) LIKE %s
-	            ORDER BY LOWER(meta_value)
-	            ",
-	            'name_%_family_name',
-	            strtolower($letter) . '%' //family names starting with our letter
-	        ));
-
-			$counter = 0;
-			$number_of_rows = count($rows);
-
-	        //print_r($rows);   ?>
-		<div class="grid-container peopleNames">
-		<?php	// loop through the results
-			if( !empty($rows) ) :
-
-				foreach( $rows as $row ) : //rows contain just post ids for our people
-
-						//Get the person
-						$person = get_post($row->post_id);
-
-						$name = get_field('name',$person->ID);
-						//print_r($name);
-
-						$images = get_field('images',$person->ID);
-						//print_r($images);
-
-						//etc - see http://new.knowledgebank.org.nz/wp-admin/post.php?post=36254&action=edit for more field names
-						$counter++;
-					?>
-
-							<div class="grid-4 peopleList">
-
-									<a href="<?php echo get_permalink( $person->ID ); ?>">
-										<li><?php echo $name[0]['family_name'] . ', ' . $name[0]['first_name'] . ' ' . $name[0]['middle_names'] ?></li>
-									</a>
-
-
-								<?php if(!empty($images) && isset($images[0]['image']['sizes']['thumbnail'])): ?>
-
-										<a href="<?php echo get_permalink( $person->ID ); ?>">
-											<img src="<?php echo $images[0]['image']['sizes']['thumbnail']; ?>" />
-										</a>
-
-								<?php endif; ?>
-
-							</div>
-
-
-					<?php if ( $counter % 3 == 0 && $counter != $number_of_rows) : ?>
-
-        				</div><div class="grid-container peopleNames">
-
-        			<?php endif; ?>
-
-				<?php
-
-
-				endforeach; //foreach($rows)
-
-
-			endif; //if($rows) ?>
-
-		</div> <!-- close grid-container div -->
-
-		<?php endif; //if($letter)
-
-	?>
-		<h3>Latest Posts</h3>
-
-	</div>
+<div class="grid-container">
 
 	<?php
 
-		$args = array(
+		if( !empty( $latest_posts ) ) :
 
-			'post_type' => 'person',
+			 foreach( $latest_posts as $latest_post ) : ?>
 
-			'posts_per_page' => 5
+			 	<div class="grid-1-5 image-subjects-links">
 
-		);
+			 		<a href='<?php echo get_permalink( $latest_post->ID ); ?>'>
 
-		$latest_posts = get_posts($args);
+			 			<?php echo $latest_post->post_title; ?>
 
-	?>
+			 		</a>
 
-	<!-- <div class="grid-container">
+			 	</div>
+
+			<?php endforeach;
+
+		endif; ?>
+
+</div>
+
+<div class="container">
+
+	<?php
+	//identify rows that have the matching first letter of family name with
+	//the letter that is clicked on
+
+	//set $letter to our GET variable if that variable is set, otherwise false
+	$letter = ( isset($_GET['letter']) ? $_GET['letter'] : false );
+
+
+	if($letter) :
+
+		//match family_name on our letter, with both coverted to lowercase in case either is uppercase
+
+		$rows = $wpdb->get_results($wpdb->prepare(
+            "
+            SELECT post_id
+            FROM {$wpdb->prefix}postmeta
+            WHERE meta_key LIKE %s
+                AND LOWER(meta_value) LIKE %s
+            ORDER BY LOWER(meta_value)
+            ",
+            'name_%_family_name',
+            strtolower($letter) . '%' //family names starting with our letter
+        ));
+
+		$counter = 0;
+		$number_of_rows = count($rows);
+
+        //print_r($rows);   ?>
+	<div class="grid-container peopleNames">
+	<?php	// loop through the results
+		if( !empty($rows) ) :
+
+			foreach( $rows as $row ) : //rows contain just post ids for our people
+
+				//Get the person
+				$person = get_post($row->post_id);
+
+				$name = get_field('name',$person->ID);
+				//print_r($name);
+
+				$images = get_field('images',$person->ID);
+				//print_r($images);
+
+				//etc - see http://new.knowledgebank.org.nz/wp-admin/post.php?post=36254&action=edit for more field names
+				$counter++;
+			?>
+
+			<div class="grid-4 peopleList">
+
+				<a href="<?php echo get_permalink( $person->ID ); ?>">
+					<li><?php echo $name[0]['family_name'] . ', ' . $name[0]['first_name'] . ' ' . $name[0]['middle_names'] ?></li>
+				</a>
+
+
+				<?php if(!empty($images) && isset($images[0]['image']['sizes']['thumbnail'])): ?>
+
+					<a href="<?php echo get_permalink( $person->ID ); ?>">
+						<img src="<?php echo $images[0]['image']['sizes']['thumbnail']; ?>" />
+					</a>
+
+				<?php endif; ?>
+
+			</div>
+
+
+			<?php if ( $counter % 3 == 0 && $counter != $number_of_rows) : ?>
+
+				</div><div class="grid-container peopleNames">
+
+			<?php endif; ?>
 
 		<?php
 
-			if( !empty( $latest_posts ) ) :
+		endforeach; //foreach($rows)
 
-				 foreach( $latest_posts as $latest_post ) : ?>
+		endif; //if($rows) ?>
 
-				 	<div class="grid-1-5 image-subjects-links">
+	</div> <!-- close grid-container div -->
 
-				 		<a href='<?php echo get_permalink( $latest_post->ID ); ?>'>
+	<?php endif; //if($letter)
 
-				 			<?php echo $latest_post->post_title; ?>
+?>
+	
 
-				 		</a>
+</div>
 
-				 	</div>
-
-				<?php endforeach;
-
-			endif; ?>
-
-	</div> -->
+	
 
 	<ul class="grid-container namesList link-hover peopleList">
 
