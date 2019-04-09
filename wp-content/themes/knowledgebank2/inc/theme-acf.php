6381204a07c02b03b6474105773ec4db9cde3e9e
ChrisWebbNZ <espiex@gmail.com>
2019-04-09 19:11:46 +1200

    Master field upload location

--- wp-content/themes/knowledgebank2/inc/theme-acf.php
+++ wp-content/themes/knowledgebank2/inc/theme-acf.php
@@ -1,18 +1,20 @@
 <?php
-/*
+
 //change upload dir for records
 function knowledgebank_acf_upload_prefilter( $errors, $file, $field ) {
     //this filter changes directory just for item being uploaded
-    add_filter('upload_dir', 'knowledgebank_upload_directory');
+    add_filter('cud_generate_path', 'knowledgebank_master_upload_directory',10,2);
+
     return $errors;
 }
-//add_filter('acf/upload_prefilter/name=master', 'knowledgebank_acf_upload_prefilter',10,3);
+add_filter('acf/upload_prefilter/name=master', 'knowledgebank_acf_upload_prefilter',10,3);
 
-function knowledgebank_master_upload_directory( $param ){
-    global $post;
+function knowledgebank_master_upload_directory( $dir, $post_id){
+
+    $filename = basename($dir);
 
     //create /node/ and /node/master/ dirs if needed
-    $dir = "/node/{$post->ID}";
+    $dir = "/node/$post_id";
     if(!file_exists($param['basedir'] . $dir)){
         mkdir($param['basedir'] . $dir);
     }
@@ -21,12 +23,8 @@ function knowledgebank_master_upload_directory( $param ){
         mkdir($param['basedir'] . $dir);
     }
 
-    $param['path'] = $param['basedir'] . $dir;
-    $param['url'] = $param['baseurl'] . $dir;
-
-    return $param;
+    return $dir;// . $filename;
 }
-*/
 
 
 function knowledgebank_collections_hierarchy( $args, $field, $post_id ){
