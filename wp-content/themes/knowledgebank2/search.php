cc4b9708ee5e8083c0fb52f25224516c4f3f4f53
ChrisWebbNZ <espiex@gmail.com>
2020-02-24 20:12:51 +1300

    Nicer search results

--- wp-content/themes/knowledgebank2/search.php
+++ wp-content/themes/knowledgebank2/search.php
@@ -33,17 +33,81 @@
 
 				<main role="main">
 					<!-- section -->
-					<section class="layer">
-						<div class="inner thin content">
+					<section class="layer results tiles search-results">
+						<div class="inner">
 
-							<?php if(!empty($search_query)): ?><h3>Results for <em><?php echo $search_query; ?></em></h3><?php endif; ?>
+							<div class="grid column-4 ">
 
-							<?php get_template_part('search_loop'); ?>
-				
+								<?php if(have_posts()): while(have_posts()): the_post(); ?>
+									<?php if($post->post_status != 'publish' || $post->ID == 113310) continue; ?>
+									<?php
+
+										$type = $post->post_type;
+										$images = get_field('images', $post->ID);
+										$image = !empty($images[0]['image']) ? $images[0]['image'] : false;
+										if(empty($image)) $image = get_field('image');
+
+
+										$link = get_permalink($post->ID);
+										$image_size = 'thumbnail';
+										if(is_tax() && ($term->term_id == 967 || $term->parent == 967)) $image_size = 'medium';//medium for photo news
+									?>
+
+									<div class="col tile shadow <?php echo $type; ?>">
+
+										<?php
+											$src = '/wp-content/themes/knowledgebank2/img/placeholder-400.png'; //default
+											if($post->post_type == 'video' && get_field('youtube_id', $post->ID)) $src = sprintf('https://img.youtube.com/vi/%s/0.jpg', get_field('youtube_id', $post->ID));
+											if(!empty($image['sizes'][$image_size])) $src = $image['sizes'][$image_size];
+										?>
+
+										<div class="tile-img lazy" style="background-image:url(/wp-content/themes/knowledgebank2/img/placeholder-400.png)" data-src="<?php echo $src; ?>">
+											<a href="<?php echo $link; ?>"></a>
+										</div>
+
+										<div class="tile-copy">
+											<h4><a href="<?php echo $link; ?>"><?php echo $post->post_title; ?></a></h4>
+												<?php get_template_part('sections/post-breadcrumbs'); ?>
+												<?php //the_excerpt(); ?>
+												<?php
+													if($type == 'audio') { //see if we have an mp3 and/or ogg
+														$mp3 = get_field('audio', $post->ID);
+														$ogg = false;
+														$master = get_field('master', $post->ID);
+														if(!empty($master['mime_type']) && $master['mime_type'] == 'audio/ogg'){
+															$ogg = $master['url'];
+														}
+														if(!empty($mp3) || !empty($ogg)): ?>
+														<audio controls>
+														  <?php if($mp3): ?><source src="<?php echo $mp3['url']; ?>" type="<?php echo $mp3['mime_type']; ?>"><?php endif; ?>
+														  <?php if($ogg): ?><source src="<?php echo $ogg; ?>" type="audio/ogg"><?php endif; ?>
+														  Your browser does not support the audio element.
+														</audio>
+														<?php endif;
+													}
+
+												?>
+											<div class="button-group">
+												<a href="<?php echo $link; ?>" class="button">View Details</a>
+											</div>
+										</div><!-- .tile-copy -->
+									</div><!-- .col -->
+
+									<?php endwhile; ?>
+
+								<?php else: ?>
+
+									<p>No results found</p>
+
+								<?php endif; ?>
+
+							</div><!-- .grid -->
+
 							<ul class="pagination">
 								<?php knowledgebank_numeric_posts_nav(); ?>
 							</ul>
-						</div>
+
+						</div><!-- .inner -->
 					</section>
 					<!-- /section -->
 				</main>
---
+++ wp-content/themes/knowledgebank2/sections/post-breadcrumbs.php
@@ -0,0 +1,33 @@
+<ul class="breadcrumbs">
+<?php
+    //get hierarchical collection list
+    global $post;
+    $collections = knowledgebank_get_collections($post->ID);
+    //grab one... could potentially deal with multiple though
+    if(!empty($collections)){
+
+        echo sprintf('<li><a href="%s">%s</a></li> ', '/collections/', 'Collections');
+
+        $collection = array_shift($collections);
+        echo sprintf('<li><a href="%s">%s</a></li> ', get_term_link($collection->term_id), $collection->name);
+        if(!empty($collection->children)){
+            $children = $collection->children;
+            do{
+                $child = array_shift($children);
+                echo sprintf('<li><a href="%s">%s</a></li> ', get_term_link($child->term_id), $child->name);
+                $children = $child->children;
+            } while (!empty($children));
+        }
+    }
+
+    if($post->post_type == 'post'){
+        $posts_page_id = get_option('page_for_posts');
+        echo sprintf('<li><a href="%s">%s</a></li>', get_permalink($posts_page_id),get_the_title($posts_page_id));
+    }
+
+
+    if($post->post_type == 'bibliography'){
+        echo '<li><a href="/bibliography">Bibliography</a></li>';
+    }
+?>
+</ul>
