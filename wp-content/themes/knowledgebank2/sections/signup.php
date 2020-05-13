3733f863737a865bb7608fc6555031c857002390
ChrisWebbNZ <chris@mogul.co.nz>
2020-05-13 19:09:05 +1200

    Proper mailchimp form

--- wp-content/themes/knowledgebank2/sections/signup.php
+++ wp-content/themes/knowledgebank2/sections/signup.php
@@ -3,11 +3,23 @@
         <div class="section-header center">
             <h4>Sign up to our newsletter</h4>
         </div>
-        <form class="" action="https://knowledgebank.us6.list-manage.com/subscribe/post?u=987c573b82980bc4711d0b837&amp;id=26709215a8" target="_blank" method="post">
-            <input type="text" name="FNAME" value="" placeholder="First name">
-            <input type="text" name="LNAME" value="" placeholder="Last name">
-            <input type="email" name="EMAIL" value="" placeholder="Email address">
-            <button type="submit" name="button">Sign up</button>
-        </form>
+        <div id="mc_embed_signup">
+           <form action="https://knowledgebank.us6.list-manage.com/subscribe/post?u=987c573b82980bc4711d0b837&amp;id=26709215a8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+              <div id="mc_embed_signup_scroll">
+                    <input type="text" value="" name="FNAME" class="" placeholder="First Name" id="mce-FNAME">
+                    <input type="text" value="" name="LNAME" class="" placeholder="Last Name" id="mce-LNAME">
+                    <input type="email" value="" name="EMAIL" class="required email" placeholder="Email address *" id="mce-EMAIL">
+                 <div id="mce-responses" class="clear">
+                    <div class="response" id="mce-error-response" style="display:none"></div>
+                    <div class="response" id="mce-success-response" style="display:none"></div>
+                 </div>
+                 <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                 <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_987c573b82980bc4711d0b837_26709215a8" tabindex="-1" value=""></div>
+                 <div class="clear"><button type="submit" value="Sign up" name="subscribe" id="mc-embedded-subscribe" class="button">Sign Up</button></div>
+              </div>
+           </form>
+        </div>
+        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='text';fnames[4]='MMERGE4';ftypes[4]='text';fnames[5]='MMERGE5';ftypes[5]='text';fnames[6]='MMERGE6';ftypes[6]='text';fnames[7]='MMERGE7';ftypes[7]='text';fnames[8]='MMERGE8';ftypes[8]='text';fnames[9]='MMERGE9';ftypes[9]='phone';fnames[10]='MMERGE10';ftypes[10]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+        <!--End mc_embed_signup-->
     </div>
 </section>
